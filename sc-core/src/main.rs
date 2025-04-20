2411b1eb8081cd9b568734c53c000007ecf2d709
j4n-r <jan.ruegge@gmail.com>
2025-04-20 15:27:33 +0200

    add comments

--- sc-core/src/main.rs
+++ sc-core/src/main.rs
@@ -30,23 +30,27 @@ async fn handle_connection(
     raw_stream: TcpStream,
     addr: SocketAddr,
 ) {
+    // perform websocket handshae on a accepted connection
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
     println!("WebSocket connection established: {}", addr);
 
-    let (outgoing, mut incoming) = ws_stream.split();
-    // tx=transmit, rx=receive (between async tokio tasks)
+    let (ws_sink, mut ws_stream) = ws_stream.split();
+    // tx=transmit, rx=receive (tx and rx as a channel between tokio tasks)
     let (tx, rx) = unbounded();
 
-    tokio::spawn(rx.map(Ok).forward(outgoing));
+    // new tokio task which forwars all messages from rx end to the ws sink ( ws_stream -> tx -> |task bound| -> rx -> ws_sink)
+    tokio::spawn(rx.map(Ok).forward(ws_sink));
 
     let mut user_con: Option<UserConn> = None;
-    if let Some(Ok(first_msg)) = incoming.next().await {
+    // get id from the first message and save the user conn in the peer map
+    if let Some(Ok(first_msg)) = ws_stream.next().await {
         let user_msg = handle_id_message(first_msg);
         user_con = Some(add_user_conn_to_peers(user_msg.clone(), addr, tx, peer_map.clone()));
     }
-    incoming
+    // forward each message after the first to all peers
+    ws_stream
         .try_for_each(|msg| {
             let ws_msg = handle_message(msg);
             let _ = forward_to_peer(ws_msg, peer_map.clone()).unwrap();
@@ -56,6 +60,7 @@ async fn handle_connection(
         .expect("Error processing incoming messages");
     
     println!("{} disconnected", &addr);
+    // remove the connection after the client disconnects
     if let Some(user_con) = &user_con {
         peer_map.lock().unwrap().remove(user_con);
     }
@@ -72,6 +77,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", addr);
 
+    // spawn new thread for each connection
     while let Ok((stream, addr)) = listener.accept().await {
         tokio::spawn(handle_connection(state.clone(), pool.clone(), stream, addr));
     }
@@ -100,6 +106,7 @@ fn add_user_conn_to_peers(msg: IdMessage, addr: SocketAddr, tx: Tx, peer_map: Pe
     };
     let user_con_clone = user_con.clone();
     println!("Added user: {:?}", user_con);
+    // save the user id + address and the corresponding transmitter for the tokio task in the peer map
     peer_map.lock().unwrap().insert(user_con, tx);
 
     user_con_clone
